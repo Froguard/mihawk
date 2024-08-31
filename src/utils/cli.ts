f495153770a03576430d3dcff5b04a97f842991b
froguard <figure_wf@163.com>
2024-09-01 01:57:44 +0800

    feat: 0.2.0-alpha

--- src/utils/cli.ts
+++ src/utils/cli.ts
@@ -1,9 +1,4 @@
 'use strict';
-/**
- * 命令行交互式操作相关封装：
- * - 1.不导出的成员（类型定义除外），请在命名上保持私有命名风格，以下划线_开头，_xxx
- * - 2.尽可能不互相引用 utils/ 文件夹之下的其他兄弟模块，防止形成循环依赖
- */
 import prompts from 'prompts';
 import minimist from 'minimist';
 import type { CliArgs } from '../com-types';
