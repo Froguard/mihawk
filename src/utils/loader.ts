c46d825676d707ca7196b1ba1a44df5b35f0f622
froguard <figure_wf@163.com>
2024-09-01 04:38:47 +0800

    feat: init 0.2.0-alpha

--- src/utils/loader.ts
+++ src/utils/loader.ts
@@ -11,31 +11,44 @@ const cacheJson = new LRUCache<string, Record<string, any>>({ max: 50 });
 const cacheTsFileCnt = new LRUCache<string, string>({ max: 50 });
 
 interface LoadWithCacheOptions<Data = any> {
-  cacheObj: LRUCache<string, Data>;
-  loadFunc: (key: string) => Promise<Data>;
+  cacheObj: LRUCache<string, Data | string>;
+  resolveData?: (fileContent: string | null) => Promise<Data | null>;
   forceRefresh?: boolean;
 }
 /**
  * 加载数据，并缓存数据
  * - 优先读取缓存
  * - 如无缓存，则调用 loadFunc 获取最新数据，获取之后会重新甚至到缓存上
- * @param {string} cacheKey
+ * @param {string} filePath
  * @param {LoadWithCacheOptions<Data>} options
  * @param {LRUCache<string, Data>} [options.cacheObj] 缓存对象
  * @param {(key: string) => Promise<Data>} [options.loadFunc] 加载数据的函数
  * @param {boolean} [options.forceRefresh=false] 强制刷新缓存，即使缓存中存在数据
  * @returns
  */
-async function _loadWithCache<Data = any>(cacheKey: string, options: LoadWithCacheOptions<Data>) {
-  const { cacheObj, loadFunc, forceRefresh } = options;
-  let cacheData: Data | null | undefined = null;
-  if (!forceRefresh && cacheObj.has(cacheKey)) {
-    cacheData = cacheObj.get(cacheKey);
+async function _loadFileWithCache<Data = any>(filePath: string, options: LoadWithCacheOptions<Data>) {
+  const { cacheObj, resolveData, forceRefresh } = options;
+  let cacheData: Data | string | null | undefined = null;
+  if (!forceRefresh && cacheObj.has(filePath)) {
+    cacheData = cacheObj.get(filePath);
   } else {
-    cacheData = await loadFunc(cacheKey);
-    cacheData && cacheObj.set(cacheKey, cacheData);
+    let fileContent: string | null = null;
+    const isFileExist = await fsEx.exists(filePath);
+    try {
+      if (isFileExist) {
+        fileContent = await fsEx.readFile(filePath, 'utf-8');
+      }
+    } catch (error) {
+      console.error('read file failed!', filePath, error);
+    }
+    if (typeof resolveData === 'function') {
+      cacheData = await resolveData(fileContent);
+    } else {
+      cacheData = fileContent;
+    }
+    cacheData && cacheObj.set(filePath, cacheData);
   }
-  return cacheData;
+  return cacheData as Data;
 }
 
 /**
@@ -43,23 +56,19 @@ async function _loadWithCache<Data = any>(cacheKey: string, options: LoadWithCac
  * @param {string} jsonFilePath
  */
 export async function loadJson(jsonFilePath: string, noCache = false) {
-  const json = await _loadWithCache<Record<string, any>>(jsonFilePath, {
+  const json = await _loadFileWithCache<Record<string, any>>(jsonFilePath, {
     cacheObj: cacheJson,
-    loadFunc: async key => {
+    forceRefresh: noCache,
+    resolveData: async JsonStr => {
       let jsonData: Record<string, any> = {};
-      if (await fsEx.exists(key)) {
-        try {
-          const jsonStr = await fsEx.readFile(key, 'utf-8');
-          jsonData = json5.parse(jsonStr);
-        } catch (e) {
-          console.error('load & parse json file failed!', e);
-        }
-      } else {
-        console.error(`load json file failed! because file not exists: ${key}`);
+      try {
+        jsonData = JsonStr ? json5.parse(JsonStr) : {};
+      } catch (error) {
+        console.error('parse json file failed!', jsonFilePath, error);
+        jsonData = {};
       }
       return jsonData;
     },
-    forceRefresh: noCache,
   });
   return json;
 }
@@ -68,36 +77,41 @@ export async function loadJson(jsonFilePath: string, noCache = false) {
  * 加载&执行 js 文件，返回执行结果
  * @param {string} jsFilePath
  */
-export async function loadJS(jsFilePath: string, noCache = false) {
+export async function loadJS<T = any>(jsFilePath: string, noCache = false) {
   if (noCache) {
     delete require.cache[jsFilePath];
     // TODO: 删除 cache 时，需要一并删除其对应依赖模块，以及依赖它的父模块的 require.cache 缓存
   }
   // @ts-ignore
-  return require(jsFilePath); // eslint-disable-line
+  const res = require(jsFilePath); // eslint-disable-line
+  return res as T;
+}
+
+interface LoadTsOptions {
+  noCache?: boolean;
+  tsConfig?: Record<string, any>;
 }
 
 /**
  * 加载&执行 ts 文件，返回执行结果
  * @param {string} tsFilePath
+ * @param {LoadTsOptions | boolean} options
  */
-export async function loadTS(tsFilePath: string, noCache = false) {
-  const tsContent = await _loadWithCache<string>(tsFilePath, {
+export async function loadTS(tsFilePath: string, options?: LoadTsOptions | boolean) {
+  let noCache = false;
+  let tsConfig = {};
+  if (typeof options === 'boolean') {
+    noCache = options;
+  } else {
+    const { noCache: disCache, tsConfig: tsCfg } = options || {};
+    noCache = !!disCache;
+    tsConfig = tsCfg || {};
+  }
+  //
+  const tsContent = await _loadFileWithCache<string>(tsFilePath, {
     cacheObj: cacheTsFileCnt,
-    loadFunc: async key => {
-      let tsCnt = '';
-      if (await fsEx.exists(key)) {
-        try {
-          tsCnt = await fsEx.readFile(key, 'utf-8');
-        } catch (e) {
-          console.error('load ts file failed!', e);
-        }
-      } else {
-        console.error(`load ts file failed! because file not fs.exists: ${key}`);
-      }
-      return tsCnt;
-    },
     forceRefresh: noCache,
+    // resolveData: async tsFileContent => tsFileContent,
   });
   console.log('tsContent', tsContent);
   // TODO: 执行 ts 文件内容里的代码
