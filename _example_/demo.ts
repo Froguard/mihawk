c6b4eb26d2a523656e36df7871d8c4f8a4c914c5
froguard <figure_wf@163.com>
2024-11-19 00:51:44 +0800

    feat: ws

--- _example_/demo.ts
+++ _example_/demo.ts
@@ -5,28 +5,6 @@
 // import { singleSelectInCli } from '../src/utils/cli';
 // import deepMerge from 'deepmerge';
 
-import http, { IncomingMessage } from 'http';
-import WsCtrl, { WsWebSocket } from '../src/composites/websocket';
-
-const port = 8080;
-const host = '0.0.0.0';
-
-const server = http.createServer((req, res) => {
-  res.end('hello');
-});
-server.listen(port, host);
-const ws = new WsCtrl({
-  host,
-  port,
-  secure: false,
-  server,
-  // resolve: (socket: WsWebSocket, request: IncomingMessage) => {
-  //   console.log('socket', socket);
-  //   console.log('request', request);
-  // },
-});
-ws.start();
-
 // async function main() {
 //   const result = await singleSelectInCli('请选择', [
 //     { title: '1', value: 1 },
