c849400b8e75c7f3cfa05a44d2157e0d0810f51b
froguard <figure_wf@163.com>
2025-03-30 22:24:24 +0800

    feat: socket emit open event immeditly after server connection has estublished

--- src/composites/websocket.ts
+++ src/composites/websocket.ts
@@ -163,10 +163,25 @@ export default class WsCtrl {
        * socket 实例上的一系列事件（通过外部提供的函数，进行自定义实现）
        */
       resolveFunc(socket, request, { clientId, stomp });
-      // 🔥 主动触发一下 open 事件，以便于如果 resolveFunc 函数中，如果针对 socket 对象进行了监听 open 事件能够执行
-      socket.emit('open', () => {
-        Printer.log(LOGFLAG_WS, 'Socket client opened!', Colors.gray(`clientId=[${clientId}]`), Colors.gray(`time=${getTimeNowStr()}`));
-      });
+      /**
+       * 🔥 主动触发一下 open 事件，以便于如果 resolveFunc 函数中，如果针对 socket 对象进行了监听 open 事件能够执行
+       */
+      setTimeout(() => {
+        const clientId4Log = Colors.gray(`clientId=[${clientId}]`);
+        const timeNow4Log = Colors.gray(`time=${getTimeNowStr()}`);
+        if (this._wsSvr) {
+          try {
+            socket?.emit('open', () => {
+              // eslint-disable-next-line
+              Printer.log(LOGFLAG_WS, "Socket client opened! (socket.emit('open') success!)", clientId4Log, timeNow4Log); // prettier-ignore
+            });
+          } catch (error) {
+            Printer.error(LOGFLAG_WS, "Failed to exec socket.emit('open') ! ", clientId4Log, timeNow4Log, '\n', error);
+          }
+        } else {
+          Printer.warn(LOGFLAG_WS, "Cancel to exec socket.emit('open'), because connection is closed.", clientId4Log, timeNow4Log);
+        }
+      }, 0);
       //
     });
 
